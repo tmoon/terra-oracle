e410d2644f8e3681b4e3067d7868292d927d2f18
covadim <nadim@covalent.ai>
2019-05-01 13:05:37 +0600

    added linter fix and updated contant.json with base api url and api key

--- src/fetcher.js
+++ src/fetcher.js
@@ -1,34 +1,33 @@
-const config = require("../config/constant.json");
-var request = require('request-promise');
+const request = require('request-promise');
+const config = require('../config/constant.json');
+
 
 const InternalFunctions = {
-    async fetch(denoms){
-        const fetchResult = await request(config.FETCH_APIS.BASE_API);
-        return fetchResult;
-    },
-    async fetchFallback(missingDenoms){
-    
-    },
-    getMissingDenoms(denoms, fetchResult){
-    
-    }
-}
+  async fetch(denoms) {
+    const denomsString = denoms.toString();
+    const API_URL = `${config.FETCH_APIS.BASE_API.URL}?fsym=ETH&tsyms=${denomsString}?api_key=${config.FETCH_APIS.BASE_API.API_KEY}`;
+    const fetchResult = await request(API_URL);
+    return fetchResult;
+  },
+  async fetchFallback(missingDenoms) {
+    return missingDenoms;
+  },
+  getMissingDenoms(denoms, fetchResult) {
+    return { denoms, fetchResult };
+  },
+};
 
 async function fetchWithFallback(denoms) {
-    const fetchResult = await InternalFunctions.fetch(denoms);
-    const missingDenoms = InternalFunctions.getMissingDenoms(denoms, fetchResult);
-    if(missingDenoms.length !== 0){
-        const fallbackFetchResult = await InternalFunctions.fetchFallback(missingDenoms);
-        const finalFetchResult = { ...fetchResult, ...fallbackFetchResult };
-        return finalFetchResult;
-    }
-    else {
-        return fetchResult;
-    }
+  const fetchResult = await InternalFunctions.fetch(denoms);
+  const missingDenoms = InternalFunctions.getMissingDenoms(denoms, fetchResult);
+  if (missingDenoms.length !== 0) {
+    const fallbackFetchResult = await InternalFunctions.fetchFallback(missingDenoms);
+    const finalFetchResult = { ...fetchResult, ...fallbackFetchResult };
+    return finalFetchResult;
+  }
+  return fetchResult;
 }
 
 module.exports = {
-    fetchWithFallback
-}
-
-console.log(fetch)
+  fetchWithFallback,
+};

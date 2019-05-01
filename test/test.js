5c13f041e09eee01ed1b4cfb1449385d20d5da67
noor148 <abbbbb148@gmail.com>
2019-05-01 16:19:05 +0600

    added tests for fetch and fetchFallback

--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+const fetcher = require('../src/fetcher');
 const assert = require('assert');
 
 describe('Array', () => {
@@ -5,5 +6,33 @@ describe('Array', () => {
     it('should return -1 when the value is not present', () => {
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
+  });
+});
+
+describe('fetcher', () => {
+  describe('#fetch()', () => {
+    it('should return json with values given in denoms', () => {
+      let result = fetcher.fetch(['usd', 'jpy']);
+      assert.equal('ust' in result && 'jpt' in result && Object.keys(result).length === 2, true);
+    });
+    it('should return json with all seven values given in denoms', () => {
+      let result = fetcher.fetch(['usd', 'krw', 'sdr', 'cny', 'jpy', 'eur', 'gbp']);
+      assert.equal('ust' in result && 'krt' in result && 'sdt' in result && 'cnt' in result && 'jpt' in result && 'eut' in result && 'gbt' in result && Object.keys(result).length === 7, true);
+    });
+  });
+
+  describe('#fetchFallback()', () => {
+    it('should return json with values given in denoms', () => {
+      let result = fetcher.fetch(['usd', 'jpy']);
+      assert.equal('ust' in result && 'jpt' in result && Object.keys(result).length === 2, true);
+    });
+    it('should return json with all seven values given in denoms', () => {
+      let result = fetcher.fetch(['usd', 'krw', 'sdr', 'cny', 'jpy', 'eur', 'gbp']);
+      assert.equal('ust' in result && 'krt' in result && 'sdt' in result && 'cnt' in result && 'jpt' in result && 'eut' in result && 'gbt' in result && Object.keys(result).length === 7, true);
+    });
+  });
+
+  describe('getMissingDenoms function', () => {
+        
   });
 });

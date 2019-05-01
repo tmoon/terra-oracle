b635c88b06efe94adfaf981eed3b9ab76603cd16
covadim <nadim@covalent.ai>
2019-05-01 14:23:38 +0600

    p0 fetch completed

--- src/fetcher.js
+++ src/fetcher.js
@@ -3,16 +3,28 @@ const config = require('../config/constant.json');
 
 
 const InternalFunctions = {
+  resultMapper(result) {
+    const mappedResult = {};
+    const resultKeys = Object.keys(result);
+    for (let i = 0; i < resultKeys.length; i += 1) {
+      const key = resultKeys[i];
+      const newKey = `${key.toLocaleLowerCase().slice(0, -1)}t`;
+      mappedResult[newKey] = result[key];
+    }
+    return mappedResult;
+  },
   async fetch(denoms) {
     const denomsString = denoms.toString();
     const API_URL = `${config.FETCH_APIS.BASE_API.URL}?fsym=ETH&tsyms=${denomsString}?api_key=${config.FETCH_APIS.BASE_API.API_KEY}`;
-    const fetchResult = await request(API_URL);
-    return fetchResult;
+    const fetchResult = JSON.parse(await request(API_URL));
+    return InternalFunctions.resultMapper(fetchResult);
   },
   async fetchFallback(missingDenoms) {
+    // TODO
     return missingDenoms;
   },
   getMissingDenoms(denoms, fetchResult) {
+    // TODO
     return { denoms, fetchResult };
   },
 };

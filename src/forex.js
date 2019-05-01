01b645f60f2b34a03871161a2848ff455d8df765
Tarik Moon <tarik@tarikmoon.com>
2019-05-01 19:09:46 +0600

    finished the forex codes

--- src/forex.js
+++ src/forex.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 const request = require('request');
 const config = require('../config/constant.json');
 
@@ -30,19 +32,75 @@ const InternalFunctions = {
   async getDataFromAPI(denoms, apiNum) {
     const url = InternalFunctions.getAPIUrl(denoms, apiNum);
 
-    return request.get(url);
+    try {
+      const data = await request.get(url);
+      return { jsonData: data, error: false };
+    }
+    catch(e) {
+      return { jsonData: {}, error: true };
+    }
   },
 
-  async getAPI0Data(denoms) {
-    const data = InternalFunctions.getDataFromAPI(denoms, 0);
-    return data;
+  getKeyToDenoms(denoms, apiNum) {
+    let keyToDenoms = {};
+
+    for (var i = denoms.length - 1; i >= 0; i--) {
+      if (apiNum === 0) {
+        keyToDenoms[denoms[i]] = InternalFunctions.getCurrencyPairFromDenom(denoms[i]);
+      } else {
+        keyToDenoms[denoms[i]] = InternalFunctions.getCurrencyFromDenom(denoms[i]);
+      }
+    }
+
+    return keyToDenoms;
+  },
+
+  parseAPIData(denoms, data, apiNum) {
+    var finalFXData = {};
+    const keyToDenoms = InternalFunctions.getKeyToDenoms(denoms, apiNum);
+
+    if (apiNum === 0) {
+      for (let key of keyToDenoms.keys()) {
+        finalFXData[keyToDenoms[key]] = data.rates[key].rate;
+      } 
+    } else if (apiNum === 1) {
+      for (let key of keyToDenoms.keys()) {
+        finalFXData[keyToDenoms[key]] = data.rates[key];
+      }
+    } else if (apiNum === 2) {
+      for (let key of keyToDenoms.keys()) {
+        finalFXData[keyToDenoms[key]] = data[key];
+      }
+    }
+
+    return finalFXData;
+  },
+
+  async getAPIData(denoms, apiNum) {
+    const res = InternalFunctions.getDataFromAPI(denoms, apiNum);
+
+    if (!res.error) {
+      const data = res.jsonData;
+
+      return { parsedFXData: InternalFunctions.parseAPIData(denoms, data, apiNum), error: false };
+    } else {
+      return { error: true };
+    }
   },
 };
 
 async function getForexRates(denoms) {
-  return InternalFunctions.getAPI0Data(denoms);
+  const res = {
+    API0Data: InternalFunctions.getAPIData(denoms, 0),
+    API1Data: InternalFunctions.getAPIData(denoms, 1),
+    API2Data: InternalFunctions.getAPIData(denoms, 2),
+  };
+
+  return res;
 }
 
 module.exports = {
   getForexRates,
 };
+
+/* eslint-enable */

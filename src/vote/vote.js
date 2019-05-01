8821a551d69936d1ae33eea551bfc566439790ff
Nazim <nazim.001.618@gmail.com>
2019-05-01 17:17:15 +0600

    submit vote has been changed

--- src/vote/vote.js
+++ src/vote/vote.js
@@ -1,14 +1,26 @@
 const EXEC = require('child_process').exec;
 const VOTER = require('../../config/voter.json');
-const { CURRENCY_MAP } = require('../../config/constant.json');
+const { CLI_CURRENCY_MAP } = require('../../config/constant.json');
 
 module.exports = {
-  submitVote: (currency, price) => {
-    if (CURRENCY_MAP[currency] === undefined) {
-      throw new Error(`"${currency}" is not listed into Whitelist`);
+  submitVote: (voteParam) => {
+    if (Object.keys(voteParam).length !== 2) {
+      throw new Error("Parameters doesn't match with standard format.");
+    }
+    if (voteParam.denom === undefined) {
+      throw new Error('"denom" flag is missed in command');
+    }
+    if (CLI_CURRENCY_MAP[voteParam.denom] === undefined) {
+      throw new Error(`"${voteParam.denom}" is not listed into Whitelist`);
+    }
+    if (voteParam.price === undefined) {
+      throw new Error('"price" flag is missed in command');
+    }
+    if (typeof (voteParam.price) !== 'number') {
+      throw new Error('"price" must be number');
     }
 
-    const command = `terracli tx oracle vote --denom "${CURRENCY_MAP[currency]}" --price "${price}" --from ${VOTER.key}`;
+    const command = `terracli tx oracle vote --denom "${CLI_CURRENCY_MAP[voteParam.denom]}" --price "${voteParam.price}" --from ${VOTER.key}`;
 
     EXEC(command, (error, stdOut, stdErr) => {
       if (stdErr !== null) {

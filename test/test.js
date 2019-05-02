a5e46336d8164e44dc6a4ce96629fddbe05b56f5
noor148 <abbbbb148@gmail.com>
2019-05-02 16:08:03 +0600

    changed async testing

--- test/test.js
+++ test/test.js
@@ -17,22 +17,26 @@ describe('Array', () => {
 describe('fetcher', () => {
 
   describe('#fetchWithFallback()', () => {
-    it('should return json with values given in denoms', () => {
-      let result = app.fetchWithFallback(['ust', 'jpt']);
-      console.log(result)
+
+    it('should return json with values given in denoms', async () => {
+      let result = await app.fetchWithFallback(['ust', 'jpt']);
+      result = result.result;
+      console.log(result);
       assert.equal('ust' in result && 'jpt' in result && Object.keys(result).length === 2, true);
       for (var elem in result) {
         if (result.hasOwnProperty(elem)) {
-          assert.equal(typeof result[elem], 'number');
+          assert.equal(typeof result[elem] === 'number', true);
         }
       }
     });
-    it('should return json with all seven values given in denoms', () => {
-      let result = fetcher.fetchWithFallback(['ust', 'krt', 'sdt', 'cnt', 'jpt', 'eut', 'gbt']);
-      assert.equal('ust' in result && 'krt' in result && 'sdt' in result && 'cnt' in result && 'jpt' in result && 'eut' in result && 'gbt' in result && Object.keys(result).length === 7, true);
+    it('should return json with all seven values given in denoms', async () => {
+      let result = await app.fetchWithFallback(['ust', 'krt', 'cnt', 'jpt', 'eut', 'gbt']);
+      result = result.result;
+      console.log(result);
+      assert.equal('ust' in result && 'krt' in result && 'cnt' in result && 'jpt' in result && 'eut' in result && 'gbt' in result && Object.keys(result).length === 6, true);
       for (var elem in result) {
         if (result.hasOwnProperty(elem)) {
-          assert.equal(typeof result[elem], 'number');
+          assert.equal(typeof result[elem] === 'number', true);
         }
       }
     });
@@ -40,26 +44,27 @@ describe('fetcher', () => {
 
   describe('#getMedian()', () => {
     it('should return median', () => {
-      assert.equal(fetcher.median([9, 3, 10]), 9);
+      assert.equal(fetcher.getMedian([9, 3, 10]), 9);
     });
     it('should return median', () => {
-      assert.equal(fetcher.median([8, 10.3, 6.1, 8]), 8);
+      assert.equal(fetcher.getMedian([8, 10.3, 6.1, 8]), 8);
     });
     it('should return median', () => {
-      assert.equal(fetcher.median([8, 10.3, 6.1, 9]), 8.5);
+      assert.equal(fetcher.getMedian([8, 10.3, 6.1, 9]), 8.5);
     });
     it('should return median', () => {
-      assert.equal(fetcher.median([8, 10.3, 6.1, 9, 8.6]), 8.6);
+      assert.equal(fetcher.getMedian([8, 10.3, 6.1, 9, 8.6]), 8.6);
     });
     it('should return median', () => {
-      assert.equal(fetcher.median([0.1, 0.001, 5, 0.963]), 0.5315);
+      assert.equal(fetcher.getMedian([0.1, 0.001, 5, 0.963]), 0.5315);
     });
   });
 
   describe('#getForexExchangeRates()', () => {
-    it('should return json with values given in denoms', () => {
-      let result = fetcher.getForexExchangeRates(['ust', 'jpt']);
+    it('should return json with values given in denoms', async () => {
+      let result = await fetcher.getForexExchangeRates(['ust', 'jpt']);
       assert.equal('ust' in result && 'jpt' in result && Object.keys(result).length === 2, true);
+      result = result.result;
       for (var elem in result) {
         if (result.hasOwnProperty(elem)) {
           assert.equal(typeof result[elem], 'number');
@@ -67,9 +72,10 @@ describe('fetcher', () => {
       }
     });
 
-    it('should return json with all seven values given in denoms', () => {
-      let result = fetcher.getForexExchangeRates(['ust', 'krt', 'sdt', 'cnt', 'jpt', 'eut', 'gbt']);
-      assert.equal('ust' in result && 'krt' in result && 'sdt' in result && 'cnt' in result && 'jpt' in result && 'eut' in result && 'gbt' in result && Object.keys(result).length === 7, true);
+    it('should return json with all seven values given in denoms', async () => {
+      let result = await fetcher.getForexExchangeRates(['ust', 'krt', 'cnt', 'jpt', 'eut', 'gbt']);
+      assert.equal('ust' in result && 'krt' in result && 'cnt' in result && 'jpt' in result && 'eut' in result && 'gbt' in result && Object.keys(result).length === 6, true);
+      result = result.result;
       for (var elem in result) {
         if (result.hasOwnProperty(elem)) {
           assert.equal(typeof result[elem], 'number');

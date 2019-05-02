aee6fbdd23a1903c8dc07d138e707e87a51e5a24
noor148 <abbbbb148@gmail.com>
2019-05-02 12:46:54 +0600

    added getForexExchangeRates

--- test/test.js
+++ test/test.js
@@ -10,44 +10,65 @@ describe('Array', () => {
 });
 
 describe('fetcher', () => {
-  describe('#fetch()', () => {
-    it('should return json with values given in denoms', () => {
-      let result = fetcher.fetch(['usd', 'jpy']);
-      assert.equal('ust' in result && 'jpt' in result && Object.keys(result).length === 2, true);
-    });
-    it('should return json with all seven values given in denoms', () => {
-      let result = fetcher.fetch(['usd', 'krw', 'sdr', 'cny', 'jpy', 'eur', 'gbp']);
-      assert.equal('ust' in result && 'krt' in result && 'sdt' in result && 'cnt' in result && 'jpt' in result && 'eut' in result && 'gbt' in result && Object.keys(result).length === 7, true);
-    });
-  });
-
-  describe('#fetchFallback()', () => {
-    it('should return json with values given in denoms', () => {
-      let result = fetcher.fetch(['usd', 'jpy']);
-      assert.equal('ust' in result && 'jpt' in result && Object.keys(result).length === 2, true);
-    });
-    it('should return json with all seven values given in denoms', () => {
-      let result = fetcher.fetch(['usd', 'krw', 'sdr', 'cny', 'jpy', 'eur', 'gbp']);
-      assert.equal('ust' in result && 'krt' in result && 'sdt' in result && 'cnt' in result && 'jpt' in result && 'eut' in result && 'gbt' in result && Object.keys(result).length === 7, true);
-    });
-  });
 
   describe('#fetchWithFallback()', () => {
     it('should return json with values given in denoms', () => {
-      let result = fetcher.fetch(['usd', 'jpy']);
+      let result = fetcher.fetchWithFallback(['ust', 'jpt']);
       assert.equal('ust' in result && 'jpt' in result && Object.keys(result).length === 2, true);
+      for (var elem in result) {
+        if (result.hasOwnProperty(elem)) {
+          assert.equal(typeof result[elem], 'number');
+        }
+      }
     });
     it('should return json with all seven values given in denoms', () => {
-      let result = fetcher.fetch(['usd', 'krw', 'sdr', 'cny', 'jpy', 'eur', 'gbp']);
+      let result = fetcher.fetchWithFallback(['ust', 'krt', 'sdt', 'cnt', 'jpt', 'eut', 'gbt']);
       assert.equal('ust' in result && 'krt' in result && 'sdt' in result && 'cnt' in result && 'jpt' in result && 'eut' in result && 'gbt' in result && Object.keys(result).length === 7, true);
+      for (var elem in result) {
+        if (result.hasOwnProperty(elem)) {
+          assert.equal(typeof result[elem], 'number');
+        }
+      }
+    });
+  });
+
+  describe('#getMedian()', () => {
+    it('should return median', () => {
+      assert.equal(fetcher.median([9, 3, 10]), 9);
+    });
+    it('should return median', () => {
+      assert.equal(fetcher.median([8, 10.3, 6.1, 8]), 8);
+    });
+    it('should return median', () => {
+      assert.equal(fetcher.median([8, 10.3, 6.1, 9]), 8.5);
+    });
+    it('should return median', () => {
+      assert.equal(fetcher.median([8, 10.3, 6.1, 9, 8.6]), 8.6);
+    });
+    it('should return median', () => {
+      assert.equal(fetcher.median([0.1, 0.001, 5, 0.963]), 0.5315);
+    });
+  });
+
+  describe('#getForexExchangeRates()', () => {
+    it('should return json with values given in denoms', () => {
+      let result = fetcher.getForexExchangeRates(['ust', 'jpt']);
+      assert.equal('ust' in result && 'jpt' in result && Object.keys(result).length === 2, true);
+      for (var elem in result) {
+        if (result.hasOwnProperty(elem)) {
+          assert.equal(typeof result[elem], 'number');
+        }
+      }
+    });
+    it('should return json with all seven values given in denoms', () => {
+      let result = fetcher.getForexExchangeRates(['ust', 'krt', 'sdt', 'cnt', 'jpt', 'eut', 'gbt']);
+      assert.equal('ust' in result && 'krt' in result && 'sdt' in result && 'cnt' in result && 'jpt' in result && 'eut' in result && 'gbt' in result && Object.keys(result).length === 7, true);
+      for (var elem in result) {
+        if (result.hasOwnProperty(elem)) {
+          assert.equal(typeof result[elem], 'number');
+        }
+      }
     });
   });
 });
 
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});

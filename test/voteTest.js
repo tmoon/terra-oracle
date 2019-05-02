a03b2631af8bd523940a7b30163056d58625c161
Nazim <nazim.001.618@gmail.com>
2019-05-02 15:43:43 +0600

    pw changed into password

--- test/voteTest.js
+++ test/voteTest.js
@@ -13,31 +13,31 @@ describe('Voting Tests', () => {
     });
     it('Should return error with error message denom flag missing', () => {
       const result = submitVote({
-        demon: 'ust', price: 0.1, key: 'thisIskey', pw: 'password',
+        demon: 'ust', price: 0.1, key: 'thisIskey', password: 'password',
       });
       assert.equal(result.status === 'error' && result.message === '"denom" flag is missed in command', true);
     });
     it('Should return error with message about currency is not in white listed', () => {
       const result = submitVote({
-        denom: 'sdt', price: 90, key: 'thisIskey', pw: 'password',
+        denom: 'sdt', price: 90, key: 'thisIskey', password: 'password',
       });
       assert.equal(result.status === 'error' && result.message === '"sdt" is not listed into Whitelist', true);
     });
     it('Should return error with message about price flag is missing', () => {
       const result = submitVote({
-        denom: 'ust', pice: 90, key: 'thisIskey', pw: 'password',
+        denom: 'ust', pice: 90, key: 'thisIskey', password: 'password',
       });
       assert.equal(result.status === 'error' && result.message === '"price" flag is missed in command', true);
     });
     it('Should return error with message about price should be number', () => {
       const result = submitVote({
-        denom: 'ust', price: 'is', key: 'thisIskey', pw: 'password',
+        denom: 'ust', price: 'is', key: 'thisIskey', password: 'password',
       });
       assert.equal(result.status === 'error' && result.message === '"price" must be number', true);
     });
     it('Should return succeed', () => {
       const result = submitVote({
-        denom: 'ust', price: 114.7, key: 'thisIskey', pw: 'password',
+        denom: 'ust', price: 114.7, key: 'thisIskey', password: 'password',
       });
       assert.equal(result.status === 'succeed' && result.message === '', true);
     });

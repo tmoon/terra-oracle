0d5d82984571ee1a624ad0d231f49dd9d035cbf7
Nazim <nazim.001.618@gmail.com>
2019-05-02 14:33:58 +0600

    Output for oracle cli completed

--- src/vote.js
+++ src/vote.js
@@ -1,4 +1,5 @@
 const EXEC = require('child_process').exec;
+const CHALK = require('chalk');
 const VOTER = require('../config/voter.json');
 const { CLI_CURRENCY_MAP } = require('../config/constant.json');
 
@@ -9,30 +10,35 @@ const InternalFunctions = {
 module.exports = {
   submitVote: (voteParam) => {
     if (Object.keys(voteParam).length !== 2) {
+      console.log(CHALK.red("Parameters doesn't match with standard format."));
       return {
         status: 'error',
         message: "Parameters doesn't match with standard format.",
       };
     }
     if (voteParam.denom === undefined) {
+      console.log(CHALK.red('"denom" flag is missed in command'));
       return {
         status: 'error',
         message: '"denom" flag is missed in command',
       };
     }
     if (CLI_CURRENCY_MAP[voteParam.denom] === undefined) {
+      console.log(CHALK.red(`"${voteParam.denom}" is not listed into Whitelist`));
       return {
         status: 'error',
         message: `"${voteParam.denom}" is not listed into Whitelist`,
       };
     }
     if (voteParam.price === undefined) {
+      console.log(CHALK.red('"price" flag is missed in command'));
       return {
         status: 'error',
         message: '"price" flag is missed in command',
       };
     }
     if (typeof (voteParam.price) !== 'number') {
+      console.log(CHALK.red('"price" must be a number'));
       return {
         status: 'error',
         message: '"price" must be number',
@@ -44,17 +50,20 @@ module.exports = {
 
     EXEC(command, (error, stdOut, stdErr) => {
       if (stdErr !== null) {
+        console.log(CHALK.red(stdErr));
         return {
           status: 'error',
           message: stdErr,
         };
       }
       if (error !== null) {
+        console.log(CHALK.red(error));
         return {
           status: 'error',
           message: error,
         };
       }
+      console.log('Successfully Voted!!');
       return {
         status: 'succeed',
         message: '',
